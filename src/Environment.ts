c37419de2b59d145b0dfa0a0b7333ef684c53533
Renan GEHAN <rgehan94@gmail.com>
2018-09-29 14:48:13 +0200

    Add support for nested environments/scopes

--- src/Environment.ts
+++ src/Environment.ts
@@ -2,26 +2,44 @@ import { Token } from './Token';
 import { RuntimeError } from './Interpreter';
 
 export class Environment {
-  values: Map<string, any> = new Map();
+  enclosing: Environment;
+  values: Map<string, any>;
+
+  constructor(enclosing: Environment = null) {
+    this.enclosing = enclosing;
+    this.values = new Map();
+  }
 
   get(varToken: Token): any {
     const variableName = varToken.lexeme;
 
-    if (!this.values.has(variableName)) {
-      throw new RuntimeError(varToken, `Undefined variable '${variableName}'.`);
+    // The variable exists in this environment
+    if (this.values.has(variableName)) {
+      return this.values.get(variableName);
     }
 
-    return this.values.get(variableName);
+    // If the variable couldn't be found in this environment, look in the parent
+    if (this.enclosing) {
+      return this.enclosing.get(varToken);
+    }
+
+    throw new RuntimeError(varToken, `Undefined variable '${variableName}'.`);
   }
 
-  assign(varToken: Token, value: any) {
+  assign(varToken: Token, value: any): any {
     const variableName = varToken.lexeme;
 
-    if (!this.values.has(variableName)) {
-      throw new RuntimeError(varToken, `Undefined variable '${variableName}'.`);
+    // The variable exists in this environment
+    if (this.values.has(variableName)) {
+      return this.values.set(variableName, value);
     }
 
-    return this.values.set(variableName, value);
+    // If the variable couldn't be found in this environment, look in the parent
+    if (this.enclosing) {
+      return this.enclosing.assign(varToken, value);
+    }
+
+    throw new RuntimeError(varToken, `Undefined variable '${variableName}'.`);
   }
 
   define(name: string, value: any) {
